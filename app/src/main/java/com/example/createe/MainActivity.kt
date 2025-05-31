e109e09714430fa46ae85c2d66ac8d2f0167c40d
nagibat0rr <egor.simonov102@mail.ru>
2025-05-31 16:24:17 +0300

    the end

--- app/src/main/java/com/example/createe/MainActivity.kt
+++ app/src/main/java/com/example/createe/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.example.createe
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val colors = listOf(
+        Color.parseColor("#800080"),
+        Color.BLUE,
+        Color.GREEN
+    )
+    private var currentColorIndex = 0
+
+    private var currentNumber = 7
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val colorSquare = findViewById<View>(R.id.colorSquare)
+        val buttonChangeColor = findViewById<Button>(R.id.buttonChangeColor)
+
+        val editTextNumber = findViewById<EditText>(R.id.editTextNumber)
+        val buttonChangeNumber = findViewById<Button>(R.id.buttonChangeNumber)
+
+        colorSquare.setBackgroundColor(colors[currentColorIndex])
+
+        buttonChangeColor.setOnClickListener {
+            currentColorIndex = (currentColorIndex + 1) % colors.size
+            colorSquare.setBackgroundColor(colors[currentColorIndex])
+        }
+
+        editTextNumber.setText(currentNumber.toString())
+
+        buttonChangeNumber.setOnClickListener {
+            currentNumber += 7
+            if (currentNumber > 49) {
+                currentNumber = 7
+            }
+            editTextNumber.setText(currentNumber.toString())
+        }
     }
 }
